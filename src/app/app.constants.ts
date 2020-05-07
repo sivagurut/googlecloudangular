05f2171105c8873f5048b2beb7fe5684f169b0af
sivagurut <sivagurut@gmail.com>
2020-05-07 13:44:50 +0530

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,3 @@
-export const DEMO_API_URL = "http://35.223.213.252:8080/helloservice"
+export const DEMO_API_URL = "http://35.223.213.252:8080"
+export const HELLO_API_URL = "http://35.223.213.252:8080"
 export const API_URL = "http://34.68.174.242:8080"
