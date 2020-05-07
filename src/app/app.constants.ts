5aa0150323ec770b1e7a4f17e7d384de9b8ca8e7
sivagurut <sivagurut@gmail.com>
2020-05-07 07:32:05 -0400

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
-export const HELLO_API_URL = "https://hello-service-6xgn7czs6q-uc.a.run.app"
-export const API_URL = "https://demoservice-6xgn7czs6q-uc.a.run.app"
+export const HELLO_API_URL = "helloapi"
+export const API_URL = "apip"
