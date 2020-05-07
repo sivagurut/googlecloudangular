41f297954bb22b5a8a61fbe13208621e3de3e2bd
sivagurut <sivagurut@gmail.com>
2020-05-07 14:16:09 +0530

    Update api.service.ts

--- src/app/features/api/service/api.service.ts
+++ src/app/features/api/service/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { DEMO_API_URL,API_URL } from './../../../app.constants';
+import { API_URL,HELLO_API_URL } from './../../../app.constants';
 export class HelloWorldBean {
   constructor(public message:string){ }
 }
@@ -13,13 +13,22 @@ export class ApiService {
   constructor(private http:HttpClient) { }
 
   executeHelloWorldBeanService() {
-    return this.http.get<HelloWorldBean>(`${DEMO_API_URL}/hello-world-bean`);    
+    return this.http.get<HelloWorldBean>(`${HELLO_API_URL}/hello-world-bean`);    
+  } 
+  executeIndexService() {
+    return this.http.get<HelloWorldBean>(`${HELLO_API_URL}/index`);    
+  } 
+  executeApiService() {
+    return this.http.get<HelloWorldBean>(`${HELLO_API_URL}/api`);    
+  } 
+  executeHelloWorldApi() {
+    return this.http.get(`${HELLO_API_URL}`);    
   } 
   executeHelloWorld() {
     return this.http.get(`${API_URL}`);    
   } 
   executeHelloWorldServiceWithPathVariable(name) {
-    return this.http.get<HelloWorldBean>(`${DEMO_API_URL}/hello-world/path-variable/${name}`);    
+    return this.http.get<HelloWorldBean>(`${HELLO_API_URL}/hello-world/path-variable/${name}`);    
   }
 
 }
