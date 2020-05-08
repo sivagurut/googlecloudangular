1fbbc7bd61cd434310ca7306a3813c5aa96078ea
sivagurut <sivagurut@gmail.com>
2020-05-08 05:47:19 -0400

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
-export const HELLO_API_URL = "https://zuul-api-6xgn7czs6q-uc.a.run.app/zuulapi"
-export const API_URL = "https://zuul-api-6xgn7czs6q-uc.a.run.app/zuulapi"
+export const HELLO_API_URL = "/loads/zuulapi"
+export const API_URL = "/loads/zuulapi"
