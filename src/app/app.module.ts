e38a96ae5c834e2937c962b59e4484982e258ab9
sivagurut <sivagurut@gmail.com>
2020-05-08 21:32:07 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ import { HttpIntercepterBasicAuthService } from './shared/http/http-intercepter-
     NgbModule
   ],
   providers: [
-   //  {provide: HTTP_INTERCEPTORS, useClass: HttpIntercepterBasicAuthService, multi: true }
+     {provide: HTTP_INTERCEPTORS, useClass: HttpIntercepterBasicAuthService, multi: true }
  ],
   bootstrap: [AppComponent]
 })
