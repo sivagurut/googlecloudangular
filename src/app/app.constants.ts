83e894fb464d4b82c0c0dfa5a68ab818ff0ce357
sivagurut <sivagurut@gmail.com>
2020-05-09 06:20:47 +0530

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
-export const HELLO_API_URL = "/loads/zuulapi"
-export const API_URL = "/loads/zuulapi"
+export const HELLO_API_URL = "/marketplace/zuulapi"
+export const API_URL = "/marketplace/zuulapi"
